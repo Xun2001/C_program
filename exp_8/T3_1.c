7aecbb0421876c0d7b02b4068f241909ec96b1d8
Xun2001 <hongxiangyu2001@gmail.com>
2024-12-14 23:21:36 +0800

    exp8/T3_1

--- exp_8/T3_1.c
+++ exp_8/T3_1.c
@@ -1,74 +1,46 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
-/*
-4.1.3 4 字符串中数字的提取
-Double pointer 双指针法
-*/
 
-void copy_to_str(char *p_left, char *p_right, char *new_str)
-{
-    char add_str={'\0'};
-    int count = 0;
-    while (*p_left != *p_right) 
-    {
-        
-        add_str=*p_left;
-        strcat(new_str, &add_str);        
-        p_left++;
+int main() {
+    char input[1000];
+    char result[1000] = "";  // 存储最终的数字串
+    char temp[100];  // 存储临时的数字串
+    int i = 0, j = 0, count = 0;
+
+    // 输入一行字符串
+    fgets(input, sizeof(input), stdin);
+
+    // 遍历字符串，提取连续数字串
+    for (int k = 0; k < strlen(input); k++) {
+        if (isdigit(input[k])) {  // 如果是数字字符
+            temp[j++] = input[k];  // 加入到临时数字串中
+        } else {
+            if (j >= 2) {  // 如果临时数字串的长度大于等于2
+                temp[j] = '\0';  // 结束当前数字串
+                if (count > 0) {
+                    strcat(result, ",");  // 不是第一个数字串，加逗号
+                }
+                strcat(result, temp);  // 将数字串加入到结果字符串
+                count++;  // 计数
+            }
+            j = 0;  // 重置临时数字串
+        }
+    }
+
+    // 检查字符串结束时是否有未处理的数字串
+    if (j >= 2) {
+        temp[j] = '\0';
+        if (count > 0) {
+            strcat(result, ",");
+        }
+        strcat(result, temp);
         count++;
     }
 
-}
-void get_num(char *str, char *new_str)
-{
-    char *p_left = &str[0];
-    char *p_right = &str[0];
-    char douhao = ',';
-    int count = 0;
+    // 输出结果
+    printf("%s\n", result);
+    printf("%d\n", count);
 
-    while (*p_right)
-    {
-        if ((*p_right >= '0' && *p_right <= '9') && count == 0)
-        {
-            p_left = p_right;
-            count++;
-        }
-        else if ((*p_right >= '0' && *p_right <= '9') && count != 0)
-        {
-            count++;
-        }
-        else if(!(*p_right >= '0' && *p_right <= '9') && count >= 2)
-        {   
-            if(new_str[0]!='\0')
-                {strcat(new_str, &douhao);}
-            copy_to_str(p_left, p_right, new_str);
-            count = 0;
-        }
-        p_right++;
-    }
-
-    if ((new_str[0]!='\0') && (*p_right=='\0') && count >= 2)
-    {
-        strcat(new_str, &douhao);
-        copy_to_str(p_left, p_right, new_str);
-    }
-    else if((new_str[0]=='\0') && (*p_right=='\0') && count >= 2)
-    {
-        copy_to_str(p_left, p_right, new_str);
-    }
-}
-int main()
-{
-    char str[100]={'\0'};
-    char num_str[100]={'\0'};
-    num_str[0]='\0';
-    // char *p_num_str;
-    printf("请输入一行字符串:\n");
-    scanf("%s",str);//遇换行符中止
-    printf("输入的字符串是:%s\n",str);//确保Vscode中scanf可以正确使用
-    get_num(str,num_str);
-    printf("提取出来的数字是:%s\n",num_str);
-	// puts(str);
-	// puts(num_str);
     return 0;
 }
