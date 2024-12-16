a9e163eef3434d3c2e2c7d6120b733bb475390f0
Xun2001 <hongxiangyu2001@gmail.com>
2024-12-16 12:30:02 +0800

    Pointer_2D2

--- practice/pointer_2D2.cpp
+++ practice/pointer_2D2.cpp
@@ -13,12 +13,30 @@ int main()
     int *p1[2];//指针数组;p1是数组; 是一个包含了2个指针的数组，每个指针指向一个int类型的变量
     int (*p2)[3];//数组指针
 
-    p2=arr;
+    for(int i=0;i<2;i++)
+    {
+        p1[i]=arr[i];
+    }
+    printf("p1:\n");
     for(int i=0;i<2;i++)
     {
         for(int j=0;j<3;j++)
         {
-            printf("%d ",*(*p2+i*3+j));
+            // printf("%d ",*(*(p1+i)+j));
+            printf("%d ",*(p1[i]+j));
+            printf("%d ",(*p1)[i][j]);
+        }
+        printf("\n");
+    }
+
+    p2=arr;
+    printf("p2:\n");
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            // printf("%d ",*(*p2+i*3+j));
+            printf("%d ",*(p2[i]+j));
         }
         printf("\n");
     }
